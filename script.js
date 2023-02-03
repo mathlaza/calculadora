d93396741549917a873caeb362d5aa45d7f6dcef
Matheus Martinez <decretomusic@gmail.com>
2023-02-03 03:22:13 -0300

    Termina lógica da calculadora

--- script.js
+++ script.js
@@ -5,36 +5,37 @@ let previousOperator;
 const screen = document.querySelector('.screen');
 
 function buttonClick(value) {
-  if (isNaN(value)) {
-    handleSymbol(value);
-  } else {
-    handleNumber(value);
-  }
+  if (isNaN(value)) handleSymbol(value);
+  else handleNumber(value);
+
   screen.innerText = buffer;
 }
 
 function handleSymbol(symbol) {
   switch (symbol) {
     case 'C':
-      runningTotal = 0;
       buffer = '0';
+      runningTotal = 0;
       break;
+
     case '=':
       if (previousOperator === null) {
         return
       }
       flushOperation(parseInt(buffer));
       previousOperator = null;
-      runningTotal = 0;
       buffer = runningTotal;
+      runningTotal = 0;
       break;
+
     case '←':
       if (buffer.length === 1) {
         buffer = '0';
       } else {
-        buffer = buffer.toString(0, buffer.length - 1);
+        buffer = buffer.substring(0, buffer.length - 1);
       }
       break;
+
     case '+':
     case '−':
     case '×':
@@ -44,4 +45,41 @@ function handleSymbol(symbol) {
   }
 }
 
-export { }; // Indicates that the file is an ES module
+function handleMath(symbol) {
+  if (buffer === '0') return;
+
+  const intBuffer = parseFloat(buffer);
+
+  if (runningTotal === 0) runningTotal = intBuffer;
+  else flushOperation(intBuffer);
+
+  previousOperator = symbol;
+  buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === '+') {
+    runningTotal = Number((runningTotal + intBuffer).toFixed(3));
+  } else if (previousOperator === '−') {
+    runningTotal = Number((runningTotal - intBuffer).toFixed(3));
+  } else if (previousOperator === '×') {
+    runningTotal = Number((runningTotal * intBuffer).toFixed(3));
+  } else if (previousOperator === '÷') {
+    runningTotal = Number((runningTotal / intBuffer).toFixed(3));
+  }
+}
+
+function handleNumber(numberString) {
+  if (buffer === '0') buffer = numberString;
+  else buffer += numberString;
+}
+
+function init() {
+  document.querySelector('.calc-buttons').addEventListener('click', function (event) {
+    buttonClick(event.target.innerText);
+  })
+}
+
+init();
+
+// export {}; // Indicates that the file is an ES module
